07bf0c3efbd7b94945a3d65e9d85868327156e17
gulevskikh <gulevskih@mail.ru>
2023-06-25 17:39:49 +0300

    changed password and login: 3_6_4

--- 3_6_4.py
+++ 3_6_4.py
@@ -1,22 +1,13 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-@pytest.fixture(scope="session")
-def app():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield app
-    print("\nquit browser..")
-    browser.quit()
-
 link = "https://stepik.org/lesson/236895/step/1"
 
-def test_site_authorization(app):
+def test_site_authorization(browser):
+    browser.implicitly_wait(5)
     browser.get(link)
-    browser.find_element(By.ID, "ember33")
-    browser.find_element(By.ID, "id_login_email").send_keys("123")
-    browser.find_element(By.ID, "id_login_password").send_keys("456")
-    browser.find_element(By.TAG_NAME, "button")
-    time.sleep(30)
+    browser.find_element(By.ID, "ember33").click()
+    browser.find_element(By.ID, "id_login_email").send_keys("asdf@mail.ru")
+    browser.find_element(By.ID, "id_login_password").send_keys("qwerty!")
+    button = browser.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+    time.sleep(10)
